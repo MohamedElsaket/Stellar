872ccf62ccd4c7650ab4b2e757946bba4a617ae9
Mohamed Elsaket <158104749+MohamedElsaket@users.noreply.github.com>
2024-04-10 16:42:07 +0200

    Add files via upload

---
+++ assets/js/templete.js
@@ -0,0 +1,12 @@
+let main = document.querySelector('.main');
+let rocket = document.querySelector('.main .rocket');
+let heading = document.querySelector('.main .heading');
+
+
+setTimeout(() => {
+    rocket.style.opacity = '1'
+}, 500);
+
+setTimeout(() => {
+    heading.style.opacity = '1'
+}, 1000);
